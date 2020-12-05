d5a5feceaf4e84d8c6dda2ce1e5b76454fae6b90
naimdasb <naimdasb@gmail.com>
2020-12-04 23:19:12 -0300

    better code

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,9 @@
 const message = document.querySelector('#hello');
 const messageToRender = 'ello there!'.split('');
 
-const renderMessage = () => {
-   if(!messageToRender.length) {
-    console.log('clear')
-     clearInterval(myfn)
-   } else {
-    message.innerHTML = message.innerHTML + messageToRender.shift()
-   }
-}
+const renderMessage = () => !messageToRender.length ? clearInterval(myfn) : message.innerHTML = message.innerHTML + messageToRender.shift()
 
 const myfn = setInterval(renderMessage, 75);
-
 
 
 
